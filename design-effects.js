593376749c20f0a354e5cc634204f224513e2c93
ProbablyFine <micahjbg@gmail.com>
2026-06-30 19:12:18 +0800

    Fixed format and favicon

--- design-effects.js
+++ design-effects.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', () => {
     const header = document.getElementById('navbar');
-    
+
     if (window.scrollY > 110) {
         header.classList.add('scrolled');
     } else {
